8369e69cfbb54cadc9de2184c22d1dc60090d9a7
Crowdbotics <team@crowdbotics.com>
2020-11-30 19:03:52 +0000

    Update models

--- backend/hottest_in_da_city_23083/wsgi.py
+++ backend/hottest_in_da_city_23083/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hottest_in_da_city_23083.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hottest_in_da_city_23083.settings")
 
 application = get_wsgi_application()
